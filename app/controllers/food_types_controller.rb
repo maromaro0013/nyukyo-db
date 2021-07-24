efdcf7e6575798fb090c35cceb5d8ea481893984
maromaro0013 <maromaro0013@gmail.com>
2021-07-24 21:33:50 +0900

    FoodTypeの一覧ページを作った

---
+++ app/controllers/food_types_controller.rb
@@ -0,0 +1,5 @@
+class FoodTypesController < ApplicationController
+    def index
+        @food_types = FoodType.all
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "residents#index"
+
   resources :residents
+  resources :food_types
+
   devise_for :users
 end
