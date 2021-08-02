8013af5b447cf66dca8e8548c6d492bb92954cb7
maromaro0013 <maromaro0013@gmail.com>
2021-08-02 17:10:20 +0900

    residentの編集と削除ができるようにした

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -12,6 +12,8 @@ class ResidentsController < ApplicationController
     @resident = Resident.new(resident_params)
     if @resident.save
       redirect_to action: :index
+    else
+      render 'new'
     end
   end
 
@@ -19,8 +21,27 @@ class ResidentsController < ApplicationController
     @resident = Resident.find(params[:id])
   end
 
+  def edit
+    @food_types = FoodType.all.map { |ft| [ft.name, ft.id] }
+    @resident = Resident.find(params[:id])
+  end
+
+  def update
+    @resident = Resident.find(params[:id])
+    if @resident.update_attributes(resident_params)
+      redirect_to @resident
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Resident.find(params[:id]).destroy
+    redirect_to "/"
+  end
+
   private
   def resident_params
-    params.require(:resident).permit(:name, :kana, :gender, :birthday, :move_in_date, :address, :food_type_id)
+    params.require(:resident).permit(:name, :kana, :gender, :birthday, :move_out_date, :move_in_date, :address, :food_type_id)
   end
 end
